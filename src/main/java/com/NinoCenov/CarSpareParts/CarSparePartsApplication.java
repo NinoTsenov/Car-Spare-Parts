0d9093b0d3d20e2d0004ff885a5a0a7e742b8a88
Nino Tsenov <nino101@abv.bg>
2023-11-17 19:09:34 +0200

    Initial commit

---
+++ src/main/java/com/NinoCenov/CarSpareParts/CarSparePartsApplication.java
@@ -0,0 +1,13 @@
+package com.NinoCenov.CarSpareParts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarSparePartsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarSparePartsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/NinoCenov/CarSpareParts/CarSparePartsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.NinoCenov.CarSpareParts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarSparePartsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
