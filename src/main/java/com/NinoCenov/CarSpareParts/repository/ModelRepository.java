50a35f12b1dba743250b965af127d05158544ec6
Nino Tsenov <nino101@abv.bg>
2023-11-20 23:49:54 +0200

    Model+Part+Make improvement

--- src/main/java/com/NinoCenov/CarSpareParts/repository/ModelRepository.java
+++ src/main/java/com/NinoCenov/CarSpareParts/repository/ModelRepository.java
@@ -3,7 +3,7 @@ import com.NinoCenov.CarSpareParts.entity.model.Model;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
+
 
 
 @Repository
