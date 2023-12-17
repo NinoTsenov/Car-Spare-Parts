2904bf9df6efcf968016011bff5bd1d6535dc5d3
Nino Tsenov <nino101@abv.bg>
2023-12-17 21:38:05 +0200

    Adding Service Make Test

--- src/main/java/com/NinoCenov/CarSpareParts/controller/PartController.java
+++ src/main/java/com/NinoCenov/CarSpareParts/controller/PartController.java
@@ -1,10 +1,8 @@
 package com.NinoCenov.CarSpareParts.controller;
-
 import com.NinoCenov.CarSpareParts.dto.part.PartRequest;
 import com.NinoCenov.CarSpareParts.dto.part.PartResponse;
 import com.NinoCenov.CarSpareParts.service.PartService;
 import jakarta.validation.Valid;
-import jakarta.websocket.server.PathParam;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
